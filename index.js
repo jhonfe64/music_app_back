241503621f3c08a45f37d2ede73f2a215efc46c7
john <jhonfe649@gmail.com>
2024-01-08 16:11:40 -0500

    configuring express server

--- index.js
+++ index.js
@@ -8,4 +8,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const port = 300;
+
+app.listen(port, () => {
+  console.log("Currently running on port " + port);
+});
+
 connection();
