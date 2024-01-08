9b2e137937e08d8610312872aea6dc922473e558
john <jhonfe649@gmail.com>
2024-01-08 16:54:28 -0500

    fix middlewares

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const userRoutes = require("./routes/user");
 const app = express();
 const port = 3000;
 
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 //Routes
 app.use("/api/user", userRoutes);
 
